dbad99e6cad8b7b1b87f56fc91557b63408cd1e7
Luis García <luis_grdz@hotmail.com>
2019-11-05 23:35:51 -0600

    Video 19: problemas comunes con los callbacks

---
+++ 02-fundamentos/callbacks2.js
@@ -0,0 +1,68 @@
+let empleados = [
+    {
+        id: 1,
+        nombre: 'Fernando'
+    },
+    {
+        id: 2,
+        nombre: 'Melissa'
+    },
+    {
+        id: 3,
+        nombre: 'Izel'
+    }
+];
+
+let salarios = [
+    {
+        id: 1,
+        salario: 1000
+    },
+    {
+        id: 2,
+        salario: 2000
+    }
+];
+
+let getEmpleado = (id, callback) => {
+    let empleadoDB = empleados.find( (empleado) => {
+        return empleado.id === id
+    } );
+
+    // console.log(empleadoDB);
+    if (!empleadoDB) {
+        callback(`No existe un empleado con ID ${id}`)
+    }else{
+        callback(null, empleadoDB)
+    }
+}
+
+let getSalario = (empleado, callback) => {
+    let salarioDB = salarios.find( (salario) => {
+        return salario.id === empleado.id
+    });
+
+    if (!salarioDB) {
+        callback('No se encontro salario para el usuario ' + empleado.nombre);
+    }else{
+        callback(null, {
+            nombre: empleado.nombre,
+            salario: salarioDB.salario,
+            id: empleado.id
+        });
+    }
+}
+
+getEmpleado(3, (err, empleado) => {
+    if (err) {
+        return console.log(err);
+    }
+    // console.log(empleado);
+    getSalario(empleado, (err, resp) => {
+        if (err) {
+            return console.log(err);
+        };
+
+        console.log('El salario de '+ resp.nombre +' es de '+ resp.salario);
+    });
+});
