c2019160511f993b632259d7196f7ca9928894de
Luis García <luis_grdz@hotmail.com>
2019-11-08 23:14:29 -0600

    Video 29: requerir paquetes(y manejo de archivos con Node)

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,5 +1,16 @@
+// Requireds ever on the first line
+const fs = require('fs');
+// const fs = require('express');
+// const fs = require('./fs');
+
 let base = 2;
+let data = '';
 
 for (let i = 1; i <= 10; i++) {
-    console.log(base + ' * ' + i + ' = ' + (base * i) );    
+    data += (base + ' * ' + i + ' = ' + (base * i) + '\n');    
 }
+
+fs.writeFile('tablas/tabla-'+base+'.txt', data, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+});
