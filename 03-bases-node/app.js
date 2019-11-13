700464153e020d269870b7e2a918c041bbdb10f2
Luis García <luis_grdz@hotmail.com>
2019-11-12 23:29:21 -0600

    Video 33: usu de yargs para crear una aplicacion que recibe comandos como una consola

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,15 +1,33 @@
 // const multiplicar = require('./multiplicar/multiplicar.js');
+const argv = require('yargs')
+.command('listar', 'imprime en consola la tabla de multiplicar', {
+    base: {
+        demand: true,
+        alias: 'b'
+    },
+    limite: {
+        alias: 'l',
+        default: 10
+    }
+})
+.help()
+.argv;
+
 const { crearArchivo } = require('./multiplicar/multiplicar.js');
 
-let base = 4;
+// let base = 4;
+// let argv2 = process.argv;
 
+console.log(argv.base);
+console.log('Limite: ',argv.limite);
+// console.log(argv2);
 // console.log(module);
 // console.log(multiplicar);
 
-crearArchivo(base)
-.then( (archivo) =>{
-    console.log(`Archivo creado: ${ archivo }`);
-})
-.catch( (e) => {
-    console.log(e);
-});
+// crearArchivo(base)
+// .then( (archivo) =>{
+//     console.log(`Archivo creado: ${ archivo }`);
+// })
+// .catch( (e) => {
+//     console.log(e);
+// });
