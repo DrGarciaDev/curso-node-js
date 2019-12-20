a7209a0ed6f945e60013fc3907a46468fbfe0ae3
Luis García <luis_grdz@hotmail.com>
2019-12-19 22:01:17 -0600

    Video 42: Validaciones automáticas con yargs

---
+++ 04-por-hacer/config/yargs.js
@@ -0,0 +1,26 @@
+const argv = require('yargs')
+    .command('crear', 'Crear un elememto por hacer', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'descripcion de la tarea por hacer'
+        }
+    })
+    .command('actualizar', 'Actualizar el estaod completado de una tarea', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'descripcion de la tarea por hacer'
+        },
+        completado: {
+            default: true,
+            alias: 'c',
+            desc: 'Marca como completado o pendiente'
+        }
+    })
+    .help()
+    .argv;
+
+    module.exports = {
+        argv
+    }
