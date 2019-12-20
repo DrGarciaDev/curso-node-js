4248127824d4163bd9a1c04afb8965b9fcaef486
Luis García <luis_grdz@hotmail.com>
2019-12-19 23:33:34 -0600

    Video 48: Optimización de pequeñas partes del código

--- 04-por-hacer/config/yargs.js
+++ 04-por-hacer/config/yargs.js
@@ -1,29 +1,25 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'descripcion de la tarea por hacer'
+}
+
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completado o pendiente'
+}
+
 const argv = require('yargs')
     .command('crear', 'Crear un elememto por hacer', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'descripcion de la tarea por hacer'
-        }
+        descripcion: descripcion
     })
-    .command('actualizar', 'Actualizar el estaod completado de una tarea', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'descripcion de la tarea por hacer'
-        },
-        completado: {
-            default: true,
-            alias: 'c',
-            desc: 'Marca como completado o pendiente'
-        }
+    .command('actualizar', 'Actualizar el estado completado de una tarea', {
+        descripcion: descripcion,
+        completado:  completado
     })
     .command('borrar', 'Borra una tarea', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'descripcion de la tarea por hacer'
-        }
+        descripcion: descripcion
     })
     .help()
     .argv;
