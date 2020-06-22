348686bf362a0e82610434cae23e3d5dd55bbf5c
Luis García <luis_grdz@hotmail.com>
2020-06-21 21:44:53 -0500

    Video 66: servir contenido estatic

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -1,21 +1,24 @@
 const express = require('express')
 const app = express()
- 
-app.get('/', function (req, res) {
-    // res.send('Hello World');
 
-    let salida = {
-        nombre: 'Luis',
-        edad: 37,
-        url: req.url
-    }
+// creando un middleware
+app.use( express.static(__dirname + '/public'));
 
-    res.send(salida);
-})
+// app.get('/', function (req, res) {
+//     // res.send('Hello World');
 
-app.get('/data', function (req, res) {
-    res.send('Hello Data');
-})
+//     let salida = {
+//         nombre: 'Luis',
+//         edad: 37,
+//         url: req.url
+//     }
+
+//     res.send(salida);
+// })
+
+// app.get('/data', function (req, res) {
+//     res.send('Hello Data');
+// })
  
 app.listen(3000, () => {
     console.log('Escuchando en el puerto 3000')
